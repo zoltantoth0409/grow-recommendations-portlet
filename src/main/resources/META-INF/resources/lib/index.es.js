8a889042c6e9eb49a955abc617de39a090d9e844
alffox <alfonso.crisci.it@gmail.com>
2019-05-29 16:10:29 +0200

    GROW-831 Reduce number of cards to 3

--- src/main/resources/META-INF/resources/lib/index.es.js
+++ src/main/resources/META-INF/resources/lib/index.es.js
@@ -51,6 +51,17 @@ const mockupData = {
             "tags": ["badge", "gamification", "respect", "test1", "test2"],            
             "readCount": "626",
             "articleCategory": "Share"
+        },
+        {
+            "articleAuthor": "Gábor Ambrózy",
+            "authorAvatar": "/o/GrowRecommendationsPortlet/images/0.jpeg",
+            "createDate": "01.01.2019",
+            "articleTitle": "RB-5",
+            "articleContent":
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            "tags": ["badge", "gamification", "respect", "test1", "test2"],            
+            "readCount": "626",
+            "articleCategory": "Share"
         }
     ]
 }
@@ -70,7 +81,7 @@ class App extends React.Component {
 			<CarouselProvider
 				naturalSlideWidth={30}
 				naturalSlideHeight={20}
-                totalSlides={4}
+                totalSlides={5}
                 visibleSlides={3}
 			>
 				<Slider>
@@ -88,9 +99,9 @@ class App extends React.Component {
 									articleCategory={growCardData.articleCategory}
 								/>
 						</Slide>
-                    )}
-				</Slider>
-				<ButtonBack>Back</ButtonBack>
+					)}
+				</Slider>		
+                <ButtonBack>Back</ButtonBack>		
 				<ButtonNext>Next</ButtonNext>
 			</CarouselProvider>
 		);
